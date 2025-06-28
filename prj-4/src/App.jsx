adefe5a2f00c4f5473e752db995f9a01c7449340
fena mavani <fenamavani37@gmail.com>
2025-06-28 13:55:02 +0530

    prj-4

---
+++ prj-4/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Formrating from './Components/FormReting';
+import './App.css';
+
+function App() {
+  return (
+
+      <Formrating />
+    
+    )
+}
+
+export default App;
+
+
+
+
